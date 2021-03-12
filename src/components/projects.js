b49332184a9febe6fe7d5715c0a5af87bcecfc29
Vasyl Semak <vasylsmk1401@gmail.com>
2021-03-11 19:18:24 -0500

    update projects component

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,38 +1,21 @@
-import React, {useContext} from "react"
+import React from "react"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGithub} from '@fortawesome/free-brands-svg-icons';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import { faLink } from '@fortawesome/free-solid-svg-icons';
-import {projects} from './data'
-import {projectsRU} from './data-ru'
-import {LanguageContext}  from '../pages/index'
+import { projects } from './data'
 import './projects.scss';
 
 const Projects = () => {
 
-  const {state} = useContext(LanguageContext)
-
-  const language = state.language 
-  
-  const projectsInfo = language === "english" ? projects : projectsRU 
-  const stack = language === "english" ? 'stack' : 'стек'
- 
   return (
-      <div id="projects">
-        <img src={require("../images/project.png")} alt="" className="project-img"/>
-         {/* <div className="paragraph-title">
-            <h3>{"<<<  "}</h3> 
-            <h1>  Projects  </h1>
-            <h3>{"  >>>"}</h3>
-          </div> */}
-        <div className="project-container">
-
-          {projectsInfo.map( (el, inx) => (
-          
+    <div id="projects">
+      <img src={require("../images/project.png")} alt="" className="project-img"/>
+      <div className="project-container">
+        {
+          projects.map(el => (
             <div id={el.class} key={el.id}>
-            
               <div id={el.img}/>
               <div className="project-details">
-              
                 <div className="project-links">
                   <a href={el.gitHub} target="_blank" rel="noreferrer">
                     <FontAwesomeIcon icon={faGithub} />
@@ -41,22 +24,17 @@ const Projects = () => {
                     <FontAwesomeIcon icon={faLink} />
                   </a>
                 </div>
-              
-              <div className="project-description">{el.description}</div>
-              <div className="project-description">{el.bulletPoints.one}</div>
-              <div className="project-description">{el.bulletPoints.two}</div>
-              <div className="project-description">{el.bulletPoints.three}</div>
-              
-              <div id="project-tools"><span>{stack}: </span>{el.tools}</div>
-
+                <div className="project-description">{el.description}</div>
+                <div className="project-description">{el.bulletPoints.one}</div>
+                <div className="project-description">{el.bulletPoints.two}</div>
+                <div className="project-description">{el.bulletPoints.three}</div>
+                <div id="project-tools"><span>stack: </span>{el.tools}</div>
+              </div>
             </div>
-          </div>
-        
-          ))}
-
-        </div>
+          ))
+        }
       </div>
-   
+    </div>
   )
 }
 
