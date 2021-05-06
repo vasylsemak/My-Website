5e2b04cc9b9c7adba0d02bfe86a3c698bca80ebf
Vasyl Semak <vasylsmk1401@gmail.com>
2021-05-06 12:31:39 -0400

    change theme color in gatsby.config

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,8 +31,8 @@ module.exports = {
 				name: `gatsby-starter-default`,
 				short_name: `starter`,
 				start_url: `/`,
-				background_color: `#663399`,
-				theme_color: `#663399`,
+				background_color: `#6b6888`,
+				theme_color: `#6b6888`,
 				display: `minimal-ui`,
 				icon: `src/images/logo-icon.png`
 			}
